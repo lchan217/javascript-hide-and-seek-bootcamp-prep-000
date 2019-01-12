62fe8906fcacd06dd2e54938dd2878dedb83485a
Elizabeth Chan <lchan217@gmail.com>
2019-01-12 02:52:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var lis = document.querySelectorAll('.ranked-list')
+ for(var i=0; lis.length>i; i++){
+   lis[i].innerHTML= (i+n).toString();
+ }
+ return lis
+}
+
+function deepestChild() {
+  var lis = document.getElementById('grand-node').querySelectorAll('div');
+  for (var i=0; lis.length>i; i++) {
+ // loopBody
+ var last = lis[i]
+  }
+  return last
+}
