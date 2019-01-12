2936c7767a75eb60a321ba7388d4bd7f807e8594
Elizabeth Chan <lchan217@gmail.com>
2019-01-12 02:04:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var lis = document.querySelectorAll('.ranked-list')
+ for(var i=0; lis.length>i; i++){
+   lis[i].innerHTML= (i+1).toString();
+ }
+ return lis
+}
+
+function deepestChild() {
+  var lis = document.querySelectorAll('grand-node');
+  return lis
+}
