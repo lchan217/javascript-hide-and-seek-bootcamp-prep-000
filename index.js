34e1943890220ba27e87bf3262cff095836a1c16
Elizabeth Chan <lchan217@gmail.com>
2019-01-12 02:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
 
 function deepestChild() {
   var lis = document.getElementById('grand-node').querySelectorAll('div');
-  while ([condition]) {
+  while (condition) {
   loopBody;
   }
   return lis
